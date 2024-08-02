d44194d17b1a894077a37ae6e1862fa656cccbeb
Fortuna Fikadu <fortunafikadu505@gmail.com>
2024-08-01 21:44:32 -0500

    add hello endpoint

---
+++ src/main/java/com/student_ms/student_ms/StudentMsApplication.java
@@ -0,0 +1,14 @@
+package com.student_ms.student_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMsApplication {
+
+	public static void main(String[] args){
+		System.out.println("Hi there");
+		 SpringApplication.run(StudentMsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/student_ms/student_ms/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.student_ms.student_ms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/v1/student")
+public class StudentController {
+
+
+    @GetMapping("/greetings")
+    public String greet() {
+        return "Hello there from Student controller";
+    }
+
+    @GetMapping("/bye")
+    public String goodBye() {
+        return "Good bye from Student controller";
+
+    }
+
+    @GetMapping("/Hello")
+    public String hello() {
+        return "Hey";
+
+    }
+
+}
